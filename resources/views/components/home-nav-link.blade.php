88a3828da766ea20a0773517888e4d198c0b6a5c
Rajesh Budhathoki <rajoyish@gmail.com>
2022-07-25 09:29:24 +0545

    Active nav link setup

--- resources/views/components/home-nav-link.blade.php
+++ resources/views/components/home-nav-link.blade.php
@@ -1,15 +1,15 @@
 <a href="{{ route('home') }}"
-    class="block py-5 px-3 transition-all duration-300 hover:bg-prime-dark hover:text-accent">Home</a>
+    class="block py-5 px-3 transition-all duration-300 hover:bg-prime-dark hover:text-accent {{ Route::currentRouteNamed('home') ?  'bg-gradient-to-b from-primary to-accent' : '' }}">Home</a>
 <a href="{{ route('about') }}"
-    class="block py-5 px-3 transition-all duration-300 hover:bg-prime-dark hover:text-accent">About
+    class="block py-5 px-3 transition-all duration-300 hover:bg-prime-dark hover:text-accent {{ Route::currentRouteNamed('about') ?  'bg-gradient-to-b from-primary to-accent' : '' }}">About
     us</a>
 <a href="{{ route('committee') }}"
-    class="block py-5 px-3 transition-all duration-300 hover:bg-prime-dark hover:text-accent">Committee</a>
+    class="block py-5 px-3 transition-all duration-300 hover:bg-prime-dark hover:text-accent {{ Route::currentRouteNamed('committee') ?  'bg-gradient-to-b from-primary to-accent' : '' }}">Committee</a>
 <a href="{{ route('objectives') }}"
-    class="block py-5 px-3 transition-all duration-300 hover:bg-prime-dark hover:text-accent">Objectives</a>
+    class="block py-5 px-3 transition-all duration-300 hover:bg-prime-dark hover:text-accent {{ Route::currentRouteNamed('objectives') ?  'bg-gradient-to-b from-primary to-accent' : '' }}">Objectives</a>
 <a href="{{ route('projects') }}"
-    class="block py-5 px-3 transition-all duration-300 hover:bg-prime-dark hover:text-accent">Projects</a>
+    class="block py-5 px-3 transition-all duration-300 hover:bg-prime-dark hover:text-accent {{ Route::currentRouteNamed('projects') ?  'bg-gradient-to-b from-primary to-accent' : '' }}">Projects</a>
 <a href="{{ route('gallery') }}"
-    class="block py-5 px-3 transition-all duration-300 hover:bg-prime-dark hover:text-accent">Gallery</a>
+    class="block py-5 px-3 transition-all duration-300 hover:bg-prime-dark hover:text-accent {{ Route::currentRouteNamed('gallery') ?  'bg-gradient-to-b from-primary to-accent' : '' }}">Gallery</a>
 <a href="{{ route('donation') }}"
-    class="block py-5 px-3 transition-all duration-300 hover:bg-prime-dark hover:text-accent">Donation</a>
+    class="block py-5 px-3 transition-all duration-300 hover:bg-prime-dark hover:text-accent {{ Route::currentRouteNamed('donation') ?  'bg-gradient-to-b from-primary to-accent' : '' }}">Donation</a>
